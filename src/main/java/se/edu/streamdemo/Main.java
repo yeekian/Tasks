9494c4c4668a8747d3838445c06d42745d77e6ab
yeekian <53704991+yeekian@users.noreply.github.com>
2024-09-23 17:33:05 +0800

    Use stream to do operations

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -14,11 +14,15 @@ public class Main {
 
         System.out.println("Printing all data ...");
         printAllData(tasksData);
+        printDataWithStreams(tasksData);
+
 
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
+        printDeadlinesUsingStream(tasksData);
 
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines usign iteration: " + countDeadlines(tasksData));
+        System.out.println("Total number of deadlines using streams: " + countDeadlinesUsingStream(tasksData));
 
     }
 
@@ -33,12 +37,21 @@ public class Main {
     }
 
     public static void printAllData(ArrayList<Task> tasksData) {
+        System.out.println("Printing data with iteration");
         for (Task t : tasksData) {
             System.out.println(t);
         }
     }
 
+    public static void printDataWithStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing data with stream");
+
+        tasks.stream()                         //Create a stream
+                .forEach(System.out::println); //terminal operator
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
+        System.out.println("Printing data with iteration");
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 System.out.println(t);
@@ -46,4 +59,18 @@ public class Main {
         }
     }
 
+    public static void printDeadlinesUsingStream(ArrayList<Task> task) {
+        System.out.println("Printing data with stream");
+        task.stream()
+                .filter(t -> t instanceof Deadline) //lambda function
+                .forEach(System.out::println);
+    }
+
+    public static int countDeadlinesUsingStream(ArrayList<Task> task) {
+        int count = (int) task.stream()
+                .filter(t -> t instanceof Deadline) //lambda function
+                .count();
+
+        return count;
+    }
 }
